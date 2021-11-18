4016cfe8f529bb67ebf1ff40135cf9e0181a77e8
Eric Wong <wongeric@mit.edu>
2021-11-18 15:42:27 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='glm_saga',
-    version='0.1.1',
+    version='0.1.2',
     description="A PyTorch solver for elastic net",
     long_description=long_description,
     long_description_content_type="text/markdown",
